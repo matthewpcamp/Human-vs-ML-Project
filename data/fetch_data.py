b571e135719a0664b98f5cb78961f6fe0ef66945
F3lixxx6705 <eastfelix@gmail.com>
2026-02-26 20:28:45 +0000

    uh NEW thingy column

--- data/fetch_data.py
+++ data/fetch_data.py
@@ -5,18 +5,22 @@ import matplotlib.pyplot as plt
 import os
 
 from ucimlrepo import fetch_ucirepo 
-  
 
 student_performance = fetch_ucirepo(id=320) 
   
+df = student_performance.variables
+
 # data (as pandas dataframes) 
 x = student_performance.data.features 
 y = student_performance.data.targets 
 
-  
+
+df['good student'] = "yes"
+
 # metadata 
 print(student_performance.metadata) 
   
 # variable information 
 print(student_performance.variables) 
 
+feature_names = student_performance.variables[student_performance.variables['role'] == 'Feature']['name'].tolist()
